b9bdc5ff5d22a6004a2dd357dc3100f0c12718a9
Tamir Razon <tamir.razon@gmail.com>
2024-05-26 17:39:52 +0300

    TamirRazon.java has changed #1

--- TamirRazon.java
+++ TamirRazon.java
@@ -1,7 +1,7 @@
 public class TamirRazon{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Tamir Razon");
+			System.out.println("Tamir Razon2");
 		}
 	}
 }
